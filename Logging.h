4bef1e7d39622f942f873720da76f1a1391008f4
adam <adam.w.bryant@outlook.com>
2017-10-28 23:44:48 -0400

    Formatted Logging.h

--- Logging.h
+++ Logging.h
@@ -1,3 +1,4 @@
-#define LOG(t,x) std::cout<<t<<"("<<__FILE__<<":"<<__LINE__<<"):"<<x<<std::endl
-#define DEBUG(x) LOG("DEBUG",x)
-#define ERROR(x) LOG("ERROR",x)
+#define LOG(t, x)                                                              \
+  std::cout << t << "(" << __FILE__ << ":" << __LINE__ << "):" << x << std::endl
+#define DEBUG(x) LOG("DEBUG", x)
+#define ERROR(x) LOG("ERROR", x)
