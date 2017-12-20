53384c3e132da77814fd45397490e01fb283451e
adam <adam.w.bryant@outlook.com>
2017-12-20 02:46:22 -0500

    Added rotation for Tetromino

--- sprite/Tetromino.cpp
+++ sprite/Tetromino.cpp
@@ -1,9 +1,6 @@
 #include "Tetromino.h"
 #include "../matrix/Matrix.h"
-#include "Mino.h"
-#include "Shapes.h"
-#include <SDL2/SDL.h>
-
+#include <algorithm>
 Tetromino::Tetromino(SDL_Renderer *mRenderer, enum Tetromino::Shape type,
                      int startX, int startY)
     : Sprite(mRenderer), gType(type) {
@@ -70,6 +67,10 @@ void Tetromino::render(int milli, SDL_Texture *targetTexture) {
       dx = -1;
     if (state[SDL_SCANCODE_D])
       dx = 1;
+    if (state[SDL_SCANCODE_Q])
+      rotate(Tetromino::Rotation::CCW);
+    if (state[SDL_SCANCODE_E])
+      rotate(Tetromino::Rotation::CW);
     if (dx != 0 || dy != 0) {
       if (Matrix::getInstance()->tryMove(this, dx, dy))
         Matrix::getInstance()->move(this, dx, dy);
@@ -85,5 +86,86 @@ bool Tetromino::moveDown() {
     return false;
   }
   Matrix::getInstance()->move(this, 0, 1);
+  return true;
+}
+void printVector(std::vector<std::vector<Mino *>> v) {
+  for (std::vector<Mino *> row : v) {
+    for (Mino *m : row) {
+      std::cout << m << ",";
+    }
+    std::cout << std::endl;
+  }
+}
+bool Tetromino::rotate(enum Tetromino::Rotation direction) {
+  struct boundingBox {
+    int left = 0;
+    int right = 0;
+    int top = 0;
+    int bottom = 0;
+    int width = 0;
+    int height = 0;
+  } box;
+  for (Mino *m : minos) {
+    SDL_Point mLocation = m->gLocation;
+    if (box.left == 0 || mLocation.x < box.left) {
+      box.left = mLocation.x;
+    }
+    if (box.right == 0 || mLocation.x > box.right) {
+      box.right = mLocation.x;
+    }
+    if (box.top == 0 || mLocation.y < box.top) {
+      box.top = mLocation.y;
+    }
+    if (box.bottom == 0 || mLocation.y > box.bottom) {
+      box.bottom = mLocation.y;
+    }
+  }
+  box.width = box.right - box.left + 1;
+  box.height = box.bottom - box.top + 1;
+  if (box.width != box.height) {
+    if (box.width < box.height) {
+      box.width = box.height;
+    } else {
+      box.height = box.width;
+    }
+  }
+  // [y][x]
+  std::vector<std::vector<Mino *>> boxToRotate(
+      box.height, std::vector<Mino *>(box.width, NULL));
+  // std::fill(boxToRotate.begin(), boxToRotate.end(), NULL);
+  for (Mino *m : minos) {
+    SDL_Point mLocation = m->gLocation;
+    boxToRotate[mLocation.y - box.top][mLocation.x - box.left] = m;
+  }
+  DEBUG("[bb] left: " << box.left << ", right: " << box.right
+                      << ", top: " << box.top << ", bottom:" << box.bottom
+                      << ", width:" << box.width << ", height:" << box.height);
+  printVector(boxToRotate);
+  std::vector<std::vector<Mino *>> transposedBox(
+      box.height, std::vector<Mino *>(box.width, NULL));
+  DEBUG("Transposed:");
+  for (unsigned int y = 0; y < transposedBox.size(); y++) {
+    for (unsigned int x = 0; x < transposedBox[y].size(); x++) {
+      transposedBox[y][x] = boxToRotate[x][y];
+    }
+  }
+  printVector(transposedBox);
+  std::vector<std::vector<Mino *>> finalBox(
+      box.height, std::vector<Mino *>(box.width, NULL));
+  if (direction == Tetromino::Rotation::CW) {
+    for (unsigned int row = 0; row < finalBox.size(); row++) {
+      finalBox[row] = transposedBox[row];
+      std::reverse(finalBox[row].begin(), finalBox[row].end());
+    }
+    DEBUG("CW");
+  } else {
+    for (unsigned int y = 0; y < finalBox.size(); y++) {
+      for (unsigned int x = 0; x < finalBox[y].size(); x++) {
+        finalBox[y][x] = transposedBox[transposedBox.size() - 1 - y][x];
+      }
+    }
+    DEBUG("CCW");
+  }
+  printVector(finalBox);
   return true;
 }
--- sprite/Tetromino.h
+++ sprite/Tetromino.h
@@ -1,17 +1,18 @@
 #pragma once
 #include "Mino.h"
 #include "Shapes.h"
-#include "Sprite.h"
 #include <SDL2/SDL.h>
 #include <vector>
 class Tetromino : public Sprite {
 
 public:
   enum Shape { O = 0, I, T, L, J, S, Z };
+  enum Rotation { CW = 0, CCW };
   Tetromino(SDL_Renderer *renderer, enum Shape type, int startX, int startY);
   virtual ~Tetromino();
   void createTexture();
   bool moveDown();
+  bool rotate(enum Rotation);
   void render(int milli, SDL_Texture *targetTexture);
   bool isActive = false;
   std::vector<Mino *> minos;
